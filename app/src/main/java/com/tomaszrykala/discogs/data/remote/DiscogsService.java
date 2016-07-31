35df7153ccbdc918a2091617ebafbcf2ca13f12f
tomaszrykala <tomasz.a.rykala@gmail.com>
2016-07-31 17:53:37 +0100

    Add demo Discogs API key and secret

--- app/src/main/java/com/tomaszrykala/discogs/data/remote/DiscogsService.java
+++ app/src/main/java/com/tomaszrykala/discogs/data/remote/DiscogsService.java
@@ -1,6 +1,5 @@
 package com.tomaszrykala.discogs.data.remote;
 
-import com.tomaszrykala.discogs.BuildConfig;
 import com.tomaszrykala.discogs.data.model.Label;
 
 import retrofit2.Call;
@@ -9,11 +8,13 @@ import retrofit2.http.Path;
 
 public interface DiscogsService {
 
-    // contstants
+    // api constants
     int MAX_RESULTS = 100;
 
-    String CONSUMER_KEY_AND_SECRET_SUFFIX =
-            "&key=" + BuildConfig.DISCOGS_CONSUMER_KEY + "&secret=" + BuildConfig.DISCOGS_CONSUMER_SECRET;
+    // demo account key & secret
+    String API_KEY = "HToYCsqnEPPlxEcMGOnR";
+    String API_SECRET = "eIwJatslNmlySmFhuZYVIxtIxbLxjICM";
+    String CONSUMER_KEY_AND_SECRET_SUFFIX = "&key=" + API_KEY + "&secret=" + API_SECRET;
 
     // api calls
     @GET("labels/333447/releases?per_page=" + MAX_RESULTS + CONSUMER_KEY_AND_SECRET_SUFFIX)
